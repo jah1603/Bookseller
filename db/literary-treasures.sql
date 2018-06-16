c02aa51d43907e67bad995a41186b9aae447243a
James Henderson <hendersonjamesalexander@gmail.com>
2018-06-16 11:42:50 +0100

    Sql runner and db

---
+++ db/literary-treasures.sql
@@ -0,0 +1,31 @@
+DROP TABLE books
+DROP TABLE publishers
+DROP TABLE genres
+
+CREATE TABLE publishers(
+  id serial8 PRIMARY KEY,
+  name varchar(255),
+  address varchar(255),
+  city varchar(255),
+  region varchar(255),
+  country VARCHAR(255),
+  postcode VARCHAR(255),
+  tel VARCHAR(255)
+);
+
+CREATE TABLE genres(
+  id serial8 PRIMARY KEY,
+  name varchar(255)
+);
+
+CREATE TABLE books(
+  id serial8 PRIMARY KEY,
+  title varchar(255),
+  author varchar(255),
+  quantity INT8,
+  publisher_id INT8 REFERENCES publishers(id) ON DELETE CASCADE,
+  genre_id INT8 REFERENCES genres(id) ON DELETE CASCADE,
+  wholesale_price DECIMAL(8, 3),
+  retail_price DECIMAL(8, 3),
+  url VARCHAR(255)
+)
