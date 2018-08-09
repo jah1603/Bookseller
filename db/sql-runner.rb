d32cc8d75b88de9745ff31d76871d305b109ad9e
James Henderson <hendersonjamesalexander@gmail.com>
2018-08-09 20:05:23 +0100

    added database name

--- db/sql-runner.rb
+++ db/sql-runner.rb
@@ -3,7 +3,7 @@ class SqlRunner
 
   def self.run( sql, values = [] )
     begin
-      db = PG.connect({ dbname: 'bookseller', host: 'ec2-54-227-241-179.compute-1.amazonaws.com', port: 5432, user: 'oznzdkxfgmejac', password: '0d49e4df2156330b1bdeb3ee08bb4b8de025e3f49a449a05d29135ef820bec4d' })
+      db = PG.connect({ dbname: 'd5j1bfhka0ncgs', host: 'ec2-54-227-241-179.compute-1.amazonaws.com', port: 5432, user: 'oznzdkxfgmejac', password: '0d49e4df2156330b1bdeb3ee08bb4b8de025e3f49a449a05d29135ef820bec4d' })
       db.prepare("query", sql)
       result = db.exec_prepared( "query", values )
     ensure
