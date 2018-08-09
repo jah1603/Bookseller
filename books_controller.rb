984604242ab3f316168ccda636ded3ef2623105c
James Henderson <hendersonjamesalexander@gmail.com>
2018-08-09 19:57:22 +0100

    a

--- books_controller.rb
+++ books_controller.rb
@@ -1,5 +1,5 @@
 require('sinatra')
-require('sinatra/contrib/all if development?')
+require('sinatra/contrib/all') if development?
 require('pry-byebug')
 require_relative('./models/books.rb')
 require_relative('./models/publishers.rb')
